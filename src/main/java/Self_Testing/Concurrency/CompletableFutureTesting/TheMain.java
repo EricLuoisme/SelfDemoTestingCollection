141a398c8692840a0b06a429e8eb8e0870d2fa0a
lykismekk <lykismekk@outlook.com>
2021-11-02 10:04:40 +0800

    Executor测试

---
+++ src/main/java/Self_Testing/Concurrency/CompletableFutureTesting/TheMain.java
@@ -0,0 +1,14 @@
+package Self_Testing.Concurrency.CompletableFutureTesting;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TheMain {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+
+    }
+}
