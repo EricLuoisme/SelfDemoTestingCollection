dbd1baaa1e7f55286413149fef96e07eaa44b581
lykismekk <lykismekk@outlook.com>
2021-11-05 17:28:02 +0800

    小修改

--- src/main/java/Self_Testing/Concurrency/ThreadSafeTests/ClassStructDemo.java
+++ src/main/java/Self_Testing/Concurrency/ThreadSafeTests/ClassStructDemo.java
@@ -1,6 +1,9 @@
 package Self_Testing.Concurrency.ThreadSafeTests;
 
+
 import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.TimeUnit;
 
 public class ClassStructDemo {
 
@@ -9,9 +12,14 @@ public class ClassStructDemo {
     public static void main(String[] args) {
         ClassStructDemo demo = new ClassStructDemo();
         System.out.println(ClassLayout.parseInstance(demo).toPrintable());
+        System.out.println("加锁后");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         synchronized (demo) {
             System.out.println(ClassLayout.parseInstance(demo).toPrintable());
         }
-        System.out.println(ClassLayout.parseInstance(demo).toPrintable());
     }
 }
